5b36fc012aca453a44362039f885d839d5c7a349
Cam Mahoney <cmahoney966@gmail.com>
2020-05-11 14:01:12 -0700

    First Save

--- classes.js
+++ classes.js
@@ -31,6 +31,21 @@
 
 //Code Here
 
+class Employee {
+  constructor(first_name, last_name, email,  age){
+    this.first_name = first_name;
+    this.last_name = last_name; 
+    this.email = email;
+    this.age = age;
+    
+  }
+}
+
+var EmployeeOne = new Employee('Camden');
+var EmployeeTwo = new Employee();
+
+console.log(EmployeeOne);
+console.log(EmployeeTwo);
 
 ////////// PROBLEM 2 //////////
 
